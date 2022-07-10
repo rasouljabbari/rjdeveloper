0f5607573f9bb4ae994d3720180955b0e1c3118c
RG Front-End <rasoul_jabbari_1377@yahoo.com>
2022-07-10 10:35:03 +0430

    ADD and set Eslint

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import './App.css';
-import Header from "./components/Header";
-import Bg from './assets/images/bg-darker.svg';
+import "./App.css"
+import Header from "./components/Header"
+import Bg from "./assets/images/bg-darker.svg"
+
 function App() {
     return (
-        <>
-            <div style={{backgroundImage: `url(${Bg})`}}
-                 className="bg-cover flex flex-col items-center justify-center min-h-screen relative">
-                <div className="container w-full">
-                    <Header/>
-                    <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
 
-                    </main>
-                </div>
+        // eslint-disable-next-line react/react-in-jsx-scope
+        <div style={{backgroundImage: `url(${Bg})`}} className="bg-cover flex flex-col items-center justify-center min-h-screen relative">
+            {/* eslint-disable-next-line react/react-in-jsx-scope */}
+            <div className="container w-full">
+                {/* eslint-disable-next-line react/react-in-jsx-scope */}
+                <Header/>
+                {/* eslint-disable-next-line react/react-in-jsx-scope */}
+                <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+                </main>
             </div>
-        </>
-    );
+        </div>
+    )
 }
 
-export default App;
+export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,24 @@
-import React from 'react';
-import Rasoul from '../assets/images/rasoul.jpeg'
+import React from "react"
+import Rasoul from "../assets/images/rasoul.jpeg"
 
-function Header(props) {
+function Header() {
     return (
         <div
-            className={'bg-gray-900 p-8 rounded-2xl relative grid grid-cols-1 lg:grid-cols-2 gap-4 m-4 shadow-md shadow-gray-900'}>
+            className={"bg-gray-900 p-8 rounded-2xl relative grid grid-cols-1 lg:grid-cols-2 gap-4 m-4 shadow-md shadow-gray-900"}>
             <div className="flex flex-wrap items-start relative pr-3 mb-6 lg:mb-0 lg:border-r border-gray-600">
                 <div className="rounded-2x mx-auto sm:mx-0 mb-4 sm:mb-0 -mt-16">
-                    <img src={Rasoul} width={162} height={162}
-                         className="rounded-3xl" alt={'rasoul jabbari'}/>
+                    <img src={Rasoul} width={162} height={162} className="rounded-3xl" alt={"rasoul jabbari"}/>
                 </div>
                 <div className="sm:ml-8">
-                    <h1 className={'text-white text-3xl mb-2'}>Rasoul Jabbari</h1>
-                    <h3 className={'text-stone-400 text-lg mb-2 hover:text-white hover:cursor-default'}>Web Developer</h3>
+                    <h1 className={"text-white text-3xl mb-2"}>Rasoul Jabbari</h1>
+                    <h3 className={"text-stone-400 text-lg mb-2 hover:text-white hover:cursor-default"}>Web
+                        Developer</h3>
                     <div className="flex items-center">
                         <div className="flex items-center">
-                            <a href={'https://www.instagram.com/accounts/login/?next=/rjdeveloper.ir/'}
-                               rel="noreferrer"
-                               target={'_blank'}>
+                            <a href={"https://www.instagram.com/accounts/login/?next=/rjdeveloper.ir/"} rel="noreferrer" target={"_blank"}>
                                 <span className="icon-instagram hover:text-pink-500 text-stone-400 text-xl pr-3"/>
                             </a>
-                            <a href={'https://wa.me/+989106878810?text=Hello Rasoul !'}
-                               rel="noreferrer"
-                               target={'_blank'}>
+                            <a href={"https://wa.me/+989106878810?text=Hello Rasoul !"} rel="noreferrer" target={"_blank"}>
                                 <span className="icon-whatsapp hover:text-green-500 text-stone-400 text-xl pr-3"/>
                             </a>
                         </div>
@@ -32,32 +28,32 @@ function Header(props) {
 
             <div className="flex flex-wrap items-start">
 
-                <ul className={'pr-4 md:pl-14'}>
-                    <li className={'lg:mb-5'}>
-                        <span className={'text-stone-400 text-sm mb-1 uppercase'}>Email</span>
-                        <p className={'text-white text-md mb-4 lg:mb-2'}>rasoul@rjdeveloper.ir</p>
+                <ul className={"pr-4 md:pl-14"}>
+                    <li className={"lg:mb-5"}>
+                        <span className={"text-stone-400 text-sm mb-1 uppercase"}>Email</span>
+                        <p className={"text-white text-md mb-4 lg:mb-2"}>rasoul@rjdeveloper.ir</p>
                     </li>
                     <li>
-                        <span className={'text-stone-400 text-sm mb-1 uppercase'}>phone</span>
-                        <p className={'text-white text-md mb-4 lg:mb-2'}>09106878810</p>
+                        <span className={"text-stone-400 text-sm mb-1 uppercase"}>phone</span>
+                        <p className={"text-white text-md mb-4 lg:mb-2"}>09106878810</p>
                     </li>
                 </ul>
 
-                <ul className={'md:pl-14'}>
-                    <li className={'lg:mb-5'}>
-                        <span className={'text-stone-400 text-sm mb-1 uppercase'}>BIRTHDAY</span>
-                        <p className={'text-white text-md mb-4 lg:mb-2'}>1998</p>
+                <ul className={"md:pl-14"}>
+                    <li className={"lg:mb-5"}>
+                        <span className={"text-stone-400 text-sm mb-1 uppercase"}>BIRTHDAY</span>
+                        <p className={"text-white text-md mb-4 lg:mb-2"}>1998</p>
                     </li>
                     <li>
-                        <span className={'text-stone-400 text-sm mb-1 uppercase'}>LOCATION</span>
-                        <p className={'text-white text-md mb-4 lg:mb-2'}>Tabriz, Firoz alley</p>
+                        <span className={"text-stone-400 text-sm mb-1 uppercase"}>LOCATION</span>
+                        <p className={"text-white text-md mb-4 lg:mb-2"}>Tabriz, Firoz alley</p>
                     </li>
                 </ul>
 
             </div>
 
         </div>
-    );
+    )
 }
 
-export default Header;
+export default Header
