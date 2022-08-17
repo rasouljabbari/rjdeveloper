9f1598e207fb295e70de30a5b6bcfb4a3c15d13d
RG Front-End <rasoul_jabbari_1377@yahoo.com>
2022-08-17 17:02:55 +0430

    cv

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,9 +11,9 @@ function Header() {
                 </div>
                 <div className="sm:ml-8">
                     <h1 className={"text-white text-3xl mb-2"}>Rasoul Jabbari</h1>
-                    <p className={"text-stone-400 text-lg mb-2 hover:text-white hover:cursor-default"}>Web
+                    <p className={"text-stone-400 text-lg mb-2 hover:text-white hover:cursor-default"}>React Js
                         Developer</p>
-                    <div className="flex items-center">
+                    <div className="flex items-center mb-3">
                         <div className="flex items-center">
                             <a href={"https://www.instagram.com/accounts/login/?next=/rjdeveloper.ir/"} rel="noreferrer" target={"_blank"}>
                                 <span className="icon-instagram hover:text-pink-500 text-stone-400 text-xl pr-3"/>
@@ -23,6 +23,12 @@ function Header() {
                             </a>
                         </div>
                     </div>
+
+                    <a href="../../public/RjdeveloperCV.pdf" download className={"flex justify-center items-center px-2 py-2 rounded-lg text-white bg-gradient-to-l from-cyan-500 to-blue-500 hover:bg-gradient-to-r"}>
+                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path></svg>
+                        <span className={"pl-4"}>Download CV</span>
+                    </a>
+
                 </div>
             </div>
 
@@ -31,7 +37,7 @@ function Header() {
                 <ul className={"pr-4 md:pl-14"}>
                     <li className={"lg:mb-5"}>
                         <span className={"text-stone-400 text-sm mb-1 uppercase"}>Email</span>
-                        <p className={"text-white text-md mb-4 lg:mb-2"}>rasoul@rjdeveloper.ir</p>
+                        <p className={"text-white text-md mb-4 lg:mb-2"}>rjdeveloper17@gmail.com</p>
                     </li>
                     <li>
                         <span className={"text-stone-400 text-sm mb-1 uppercase"}>phone</span>
