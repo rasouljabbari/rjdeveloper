5650920add680bf12af9a29399427ce1db6ce21e
RG Front-End <rasoul_jabbari_1377@yahoo.com>
2023-02-21 14:54:03 +0330

    change description

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react"
 import AboutMe from "./tab-content/AboutMe"
 import Resume from "./tab-content/Resume"
-// import Contact from "./tab-content/Contact"
+import Contact from "./tab-content/Contact"
 
 // eslint-disable-next-line react/prop-types
 function Content() {
@@ -54,26 +54,26 @@ function Content() {
                         <span className={"text-sm xl:text-lg"}>resume</span>
                     </a>
                 </li>
-                {/*<li className=" flex-auto text-center w-auto lg:w-32 xl:w-36">*/}
-                {/*    <a*/}
-                {/*        className={*/}
-                {/*            "uppercase flex flex-col px-2 py-4 lg:p-4 xl:p-6 rounded-lg lg:rounded-2xl " +*/}
-                {/*            (openTab === 3*/}
-                {/*                ? "text-white bg-gradient-to-l from-cyan-500 to-blue-500"*/}
-                {/*                : "text-stone-300 bg-gray-800 hover:text-sky-500")*/}
-                {/*        }*/}
-                {/*        onClick={e => {*/}
-                {/*            e.preventDefault()*/}
-                {/*            setOpenTab(3)*/}
-                {/*        }}*/}
-                {/*        data-toggle="tab"*/}
-                {/*        href="#link3"*/}
-                {/*        role="tablist"*/}
-                {/*    >*/}
-                {/*        <span className="icon-phone text-2xl lg:text-4xl lg:pb-3"/>*/}
-                {/*        <span className={"text-sm xl:text-lg"}>contact</span>*/}
-                {/*    </a>*/}
-                {/*</li>*/}
+                <li className=" flex-auto text-center w-auto lg:w-32 xl:w-36">
+                    <a
+                        className={
+                            "uppercase flex flex-col px-2 py-4 lg:p-4 xl:p-6 rounded-lg lg:rounded-2xl " +
+                            (openTab === 3
+                                ? "text-white bg-gradient-to-l from-cyan-500 to-blue-500"
+                                : "text-stone-300 bg-gray-800 hover:text-sky-500")
+                        }
+                        onClick={e => {
+                            e.preventDefault()
+                            setOpenTab(3)
+                        }}
+                        data-toggle="tab"
+                        href="#link3"
+                        role="tablist"
+                    >
+                        <span className="icon-phone text-2xl lg:text-4xl lg:pb-3"/>
+                        <span className={"text-sm xl:text-lg"}>contact</span>
+                    </a>
+                </li>
             </ul>
             <div className="relative flex flex-col min-w-0 w-full lg:w-5/6 break-words bg-gray-900 mb-6 shadow-lg bg-gray-900 p-3 md:p-8 rounded-2xl shadow-md shadow-gray-900">
                 <ul
@@ -120,26 +120,26 @@ function Content() {
                             <span className={"text-sm xl:text-lg"}>resume</span>
                         </a>
                     </li>
-                    {/*<li className=" flex-auto text-center w-1/3 lg:w-32 xl:w-36">*/}
-                    {/*    <a*/}
-                    {/*        className={*/}
-                    {/*            "uppercase flex flex-col px-2 py-4 lg:p-4 xl:p-6 rounded-t-lg lg:rounded-t-2xl rounded-b-0 lg:rounded-b-0 " +*/}
-                    {/*            (openTab === 3*/}
-                    {/*                ? "text-white bg-gradient-to-b from-cyan-500 to-teal-300"*/}
-                    {/*                : "text-stone-300 bg-gray-800 hover:text-sky-500")*/}
-                    {/*        }*/}
-                    {/*        onClick={e => {*/}
-                    {/*            e.preventDefault()*/}
-                    {/*            setOpenTab(3)*/}
-                    {/*        }}*/}
-                    {/*        data-toggle="tab"*/}
-                    {/*        href="#link3"*/}
-                    {/*        role="tablist"*/}
-                    {/*    >*/}
-                    {/*        <span className="icon-phone text-2xl lg:text-4xl lg:pb-3"/>*/}
-                    {/*        <span className={"text-sm xl:text-lg"}>contact</span>*/}
-                    {/*    </a>*/}
-                    {/*</li>*/}
+                    <li className=" flex-auto text-center w-1/3 lg:w-32 xl:w-36">
+                        <a
+                            className={
+                                "uppercase flex flex-col px-2 py-4 lg:p-4 xl:p-6 rounded-t-lg lg:rounded-t-2xl rounded-b-0 lg:rounded-b-0 " +
+                                (openTab === 3
+                                    ? "text-white bg-gradient-to-b from-cyan-500 to-teal-300"
+                                    : "text-stone-300 bg-gray-800 hover:text-sky-500")
+                            }
+                            onClick={e => {
+                                e.preventDefault()
+                                setOpenTab(3)
+                            }}
+                            data-toggle="tab"
+                            href="#link3"
+                            role="tablist"
+                        >
+                            <span className="icon-phone text-2xl lg:text-4xl lg:pb-3"/>
+                            <span className={"text-sm xl:text-lg"}>contact</span>
+                        </a>
+                    </li>
                 </ul>
                 <div className="px-2 md:px-4 py-5 flex-auto">
                     <div className="tab-content tab-space">
@@ -149,9 +149,9 @@ function Content() {
                         <div className={openTab === 2 ? "block" : "hidden"} id="link2">
                             <Resume/>
                         </div>
-                        {/*<div className={openTab === 3 ? "block" : "hidden"} id="link3">*/}
-                        {/*    <Contact/>*/}
-                        {/*</div>*/}
+                        <div className={openTab === 3 ? "block" : "hidden"} id="link3">
+                            <Contact/>
+                        </div>
                     </div>
                 </div>
             </div>
